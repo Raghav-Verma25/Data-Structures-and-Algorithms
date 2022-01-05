6f59cb3b692faef8fe73471d650226e1cf5becd1
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2022-01-06 00:22:49 +0530

    NumToBin.java
    Convert number to binary and vice versa.

---
+++ NumToBin.java
@@ -0,0 +1,11 @@
+public class NumToBin {
+    public static void main(String[] args) {
+        String number =Integer.toBinaryString(9);
+        System.out.println(number);
+
+        String binaryString="1010";
+        int decimal=Integer.parseInt(binaryString,2);
+        System.out.println(decimal);
+
+    }
+}
